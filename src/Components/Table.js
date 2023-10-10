ec460d11b37702bc8822cb369d36a13a486c2b24
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2023-10-09 23:42:10 -0400

    added table component with pagination

---
+++ src/Components/Table.js
@@ -0,0 +1,95 @@
+// Table.js
+import React, { useState, useEffect } from 'react';
+
+const Table = ({ data, columns, defaultPageSize }) => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [pageSize, setPageSize] = useState(defaultPageSize);
+  const [sortedField, setSortedField] = useState(null);
+  const [sortedDirection, setSortedDirection] = useState('asc');
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const handlePageSizeChange = (e) => {
+    setPageSize(parseInt(e.target.value, 10));
+    setCurrentPage(0);
+  };
+
+  const handleSort = (field) => {
+    if (sortedField === field) {
+      setSortedDirection(sortedDirection === 'asc' ? 'desc' : 'asc');
+    } else {
+      setSortedField(field);
+      setSortedDirection('asc');
+    }
+  };
+
+  const sortedData = data.sort((a, b) => {
+    if (sortedField) {
+      const aValue = a[sortedField];
+      const bValue = b[sortedField];
+      if (aValue < bValue) return sortedDirection === 'asc' ? -1 : 1;
+      if (aValue > bValue) return sortedDirection === 'asc' ? 1 : -1;
+      return 0;
+    }
+    return 0;
+  });
+
+  const paginatedData = sortedData.slice(
+    currentPage * pageSize,
+    currentPage * pageSize + pageSize
+  );
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            {columns.map((column) => (
+              <th key={column.field} onClick={() => handleSort(column.field)}>
+                {column.label}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {paginatedData.map((item, index) => (
+            <tr
+              key={index}
+              className={index % 2 === 0 ? 'even-row' : 'odd-row'}
+            >
+              {columns.map((column) => (
+                <td key={column.field}>{item[column.field]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 0}
+        >
+          Previous
+        </button>
+        <span>
+          Page {currentPage + 1} of {Math.ceil(sortedData.length / pageSize)}
+        </span>
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === Math.ceil(sortedData.length / pageSize) - 1}
+        >
+          Next
+        </button>
+        <select onChange={handlePageSizeChange} value={pageSize}>
+          <option value={2}>2 per page</option>
+          <option value={10}>10 per page</option>
+          <option value={20}>20 per page</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Table;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,29 @@
+import Table from '../Components/Table';
+
 function Home() {
+
+    const data = [
+        {"title": "Software Engineer", "company": "Scotiabank", "status": "Applied", "date": "2023-09-28", "lastUpdated": ""},
+        {"title": "Software Developer", "company": "CRA", "status": "Pending", "date": "2023-09-26", "lastUpdated": ""},
+        {"title": "Application Developer", "company": "Bell", "status": "Applied", "date": "2023-09-21", "lastUpdated": "2023-09-27"},
+        {"title": "Full Stack Developer", "company": "CIBC", "status": "Closed", "date": "2023-09-08", "lastUpdated": "2023-09-22"},
+    ];
+
+    const columns = [
+        { label: 'Title', field: 'title' },
+        { label: 'Company', field: 'company' },
+        { label: 'Status', field: 'status' },
+        { label: 'Date', field: 'date' },
+        { label: 'Last Updated On', field: 'lastUpdated' },
+      ];
   
     return (
       <div className="home">
-        This is the home page
+        <h1>Latest applications</h1>
+        <Table data={data} columns={columns} defaultPageSize={10} />
       </div>
     );
-  }
+}
   
-  export default Home;
+export default Home;
   
